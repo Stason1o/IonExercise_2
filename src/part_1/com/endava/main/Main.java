16dc3a2491ac636eff250ab1e4d11927df93a8d9
Stason1o <stasikus1996@gmail.com>
2017-03-25 23:07:19 +0200

    small changes in creating objects

--- src/part_1/com/endava/main/Main.java
+++ src/part_1/com/endava/main/Main.java
@@ -1,23 +1,20 @@
 package part_1.com.endava.main;
 
-import part_1.com.endava.base.Product;
+import part_1.com.endava.entity.base.Product;
+import part_1.com.endava.entity.enums.ProductType;
 import part_1.com.endava.factory.OrdersFactory;
-import part_1.com.endava.util.BlackList;
-import part_1.com.endava.util.CountrySupport;
 
 /**
  * Created by sbogdanschi on 21/03/2017.
  */
 public class Main {
     public static void main(String[] args) {
-        BlackList blackList = new BlackList();
-        CountrySupport countrySupport = new CountrySupport();
 
         OrdersFactory ordersFactory = new OrdersFactory();
 
-        Product food = ordersFactory.createOrder("food", "Moldova", "Pete", blackList, countrySupport);
-        Product furniture = ordersFactory.createOrder("furniture", "USA", "Valera", blackList, countrySupport);
-        Product toys = ordersFactory.createOrder("toy", "UK", "Anton", blackList, countrySupport);
+        Product food = ordersFactory.createOrder(ProductType.FOOD, "Moldova", "Pete");
+        Product furniture = ordersFactory.createOrder(ProductType.FURNITURE, "USA", "Valera");
+        Product toys = ordersFactory.createOrder(ProductType.TOYS, "UK", "Anton");
 
         System.out.println(food);
         System.out.println(furniture);
