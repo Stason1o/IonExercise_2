29316719be8a90cb78d1ee800262def91c2f80c4
Stason1o <stasikus1996@gmail.com>
2017-03-25 23:06:42 +0200

    instead of checking for String input, check by Enum ProductType

--- src/part_1/com/endava/factory/OrdersFactory.java
+++ src/part_1/com/endava/factory/OrdersFactory.java
@@ -2,42 +2,46 @@ package part_1.com.endava.factory;
 
 import part_1.com.endava.entity.Country;
 import part_1.com.endava.entity.User;
-import part_1.com.endava.implementation.Furniture;
-import part_1.com.endava.util.BlackList;
-import part_1.com.endava.base.Product;
-import part_1.com.endava.enums.Status;
-import part_1.com.endava.implementation.Food;
-import part_1.com.endava.implementation.Toys;
-import part_1.com.endava.util.CountrySupport;
+import part_1.com.endava.entity.base.implementation.FurnitureImpl;
+import part_1.com.endava.entity.info.UsersBlackList;
+import part_1.com.endava.entity.base.Product;
+import part_1.com.endava.entity.enums.ProductType;
+import part_1.com.endava.entity.enums.Status;
+import part_1.com.endava.entity.base.implementation.FoodImpl;
+import part_1.com.endava.entity.base.implementation.ToysImpl;
+import part_1.com.endava.entity.info.CountriesBlackList;
 
 /**
  * Created by sbogdanschi on 21/03/2017.
  */
 public class OrdersFactory {
 
-    public Product createOrder(String product, String country, String user, BlackList blackList, CountrySupport countrySupport){
+    public Product createOrder(ProductType product, String country, String user){
 
-        for (User userIterator: blackList.getBannedUsers()) {
+        for (User userIterator: UsersBlackList.getBannedUsers()) {
             if (!userIterator.getName().equalsIgnoreCase(user)) {
-                for (Country countryIterator : countrySupport.getSupportedCountries()) {
+                for (Country countryIterator : CountriesBlackList.getSupportedCountries()) {
                     if (countryIterator.getName().equalsIgnoreCase(country)) {
-                        if (product.equalsIgnoreCase("food")) {
-                            return new Food(40, country, 10, 23.2, 43.2, Status.NEW);
-                        } else if (product.equalsIgnoreCase("furniture")) {
-                            return new Furniture(50, country, 15, 30.2, 76.2, Status.NEW);
-                        } else if (product.equalsIgnoreCase("toys")) {
-                            return new Toys(32.1, country, 21, 43.2, 54, Status.NEW);
+                        if (product == ProductType.FOOD) {
+                            System.out.println("Creating new " + ProductType.FOOD + " order..");
+                            return new FoodImpl(40, country, 10, 23.2, 43.2, Status.NEW);
+                        } else if (product == ProductType.FURNITURE) {
+                            System.out.println("Creating new " + ProductType.FURNITURE + " order..");
+                            return new FurnitureImpl(50, country, 15, 30.2, 76.2, Status.NEW);
+                        } else if (product == ProductType.TOYS) {
+                            System.out.println("Creating new " + ProductType.TOYS + " order..");
+                            return new ToysImpl(32.1, country, 21, 43.2, 54, Status.NEW);
                         } else {
-                            System.out.println("Cannot create a delivery");
+                            System.out.println("Creating new order...");
+                            System.out.println("Cannot create a new order");
                             return null;
                         }
                     }
                 }
-                System.out.println("Country is not supported");
+                System.out.println("Country is not supported for creating new order");
                 return null;
             }
-
-            System.out.println("User is in black list");
+            System.out.println("User is in black list and cannot create the new order");
             return null;
         }
         return null;
